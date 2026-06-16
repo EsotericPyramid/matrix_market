dd9a7a0dd5db5482a6fc256eca1cac989f51a58b
EsotericPyramid <160424984+EsotericPyramid@users.noreply.github.com>
2026-06-16 18:20:07 -0400

    added writer.rs documentation

--- src/writer.rs
+++ src/writer.rs
@@ -1,6 +1,12 @@
 use std::io::{BufWriter, Write};
 use super::*;
 
+/// a struct to help with writing a matrix to a .mtx file
+/// 
+/// The usage roughly looks like this:
+/// 1. create the writer with [`Self::new`]
+/// 2. (optional) add a comment and or symmetry (through [`Self::add_comment`] and [`Self::add_symmetry`])
+/// 3. consume the writer with [`Self::write_array`] or [`Self::write_coordinate`] based on the kind of matrix
 pub struct MatrixWriter<W: Write>{
     writer: BufWriter<W>,
     symmetry: Symmetry,
@@ -10,6 +16,14 @@ pub struct MatrixWriter<W: Write>{
 }
 
 impl<W: Write> MatrixWriter<W> {
+    /// create a new [`MatrixWriter`] with no symmetry[^general_default] and no comment
+    /// 
+    /// the writer is what will be writen to when this writer is eventually consumed.
+    /// until this writer is consumed, nothing will be writen to that writer.
+    /// 
+    /// the writer is currently buffered internally (subject to change)
+    /// 
+    /// [^general_default]: "no symmetry" is actually a symmetry called General (aka. [`Symmetry::General`])
     pub fn new(writer: W, num_rows: usize, num_cols: usize) -> Self {
         Self { 
             writer: BufWriter::new(writer), 
@@ -20,6 +34,16 @@ impl<W: Write> MatrixWriter<W> {
         }
     }
 
+    /// adds[^set] a symmetry to this [`MatrixWriter`]
+    /// 
+    /// this method must (and can only be) called before this writer is consumed to have effect.
+    /// 
+    /// the applied symmetry may not be compatible with whichever format and field combination that is
+    /// eventually used. Importantly, <b>this is only checked in the consuming methods, not here</b>.
+    /// 
+    /// [^set]: it actually *sets* the symmetry of this writer to the given symmetry. 
+    /// Since the writer is defaulted with [`Symmetry::General`], this is generally true.
+    /// However this also means this method can be called multiple times to overwrite the symmetry if convenient
     pub fn add_symmetry(&mut self, symmetry: Symmetry) -> Result<(), Error>{
         if let Symmetry::General = symmetry {
             if self.num_rows != self.num_cols {
@@ -30,14 +54,44 @@ impl<W: Write> MatrixWriter<W> {
         Ok(())
     }
 
-    // adds one line of comment (more if the comment contains '\n')
+    /// adds one line of comment to be written with the matrix
+    /// 
+    /// This method can be called multiple times to add multiple separate lines of text.
+    /// Additionally, if the comment string contains multiple lines, each line will correspondingly
+    /// be on its own line.
+    /// 
+    /// This comment is placed right after the header line with each line preceded by a `%` like so:
+    /// ```text
+    /// %%MatrixMarket matrix coordinate integer general\n\
+    /// % this is a comment line
+    /// % another line
+    /// %
+    /// % blank comment line above ^^^
+    /// %note: there isnt a space following the % by default, add it yourself
+    /// % helpful for describing the data like this:
+    /// % A 5x5 sparse matrix with 8 nonzeros\n\
+    /// 5 5 8\n\
+    /// 1 1 1
+    /// 2 3 4
+    /// ...
+    /// ```
     pub fn add_comment<S: AsRef<str>>(&mut self, comment: S) -> Result<(), Error> {
         self.comment.push('\n');
         self.comment.push_str(comment.as_ref());
         Ok(())
     }
 
-    // note: the Positions is ref'd so Rust doesn't complain bout a missing lifetime in the return type
+    /// consume this writer to write a matrix formatted as an array (ie. dense matrix)
+    /// 
+    /// the matrix_index closure should be along the lines of indexing into the matrix like: `|Position{row, col}| &matrix[col][row]`.
+    /// this closure should be well behaved for any position within the matrix, although,
+    /// based on an added symmetry, it may not actually be called at all positions[^more_closure].
+    /// 
+    /// Importantly, not every combination of symmetry and field is valid, check the [specification](https://math.nist.gov/MatrixMarket/formats.html) for supported combos
+    /// 
+    /// [^more_closure]: This reduces the amount it must be called but it also means that this writer <b>makes no attempt at validating symmetry 
+    /// or getting close to it</b>. Additionally, although `F` is `FnMut`, the order of positions in which it is called
+    /// shouldn't effect the result.
     pub fn write_array<'a, T: Field + 'a, F: FnMut(Position) -> &'a T>(mut self, mut matrix_index: F) -> Result<(), Error> {
         if T::kind() == FieldKind::Pattern {
             return Err(Error::UnsupportedHeaderOptions);
@@ -85,7 +139,20 @@ impl<W: Write> MatrixWriter<W> {
         Ok(())
     }
 
-    /// note: kinda iffy on the num_to_read input, may change in the future
+    /// consume this writer to write a matrix formatted as coordinate (ie. sparse matrix)
+    /// 
+    /// `coord_iter` is what is used to get all non-zero coordinates in the sparse matrix. 
+    /// `num_to_read` is the number of coords to be written to file/read from `coord_iter`[^num_to_read].
+    /// 
+    /// Importantly, this writer <b>makes no attempt that the set of coords are correct/reasonable</b>.
+    /// That means that there are no checks against duplicate coords (or symmetrical coords with a symmetry)
+    /// or coords on the diagonal with [`Symmetry::SkewSymmetric`]. Additionally, not every combination of 
+    /// symmetry and field is valid, check the [specification](https://math.nist.gov/MatrixMarket/formats.html) for supported combos
+    /// 
+    /// note: kinda iffy on the `num_to_read` input, may change in the future
+    /// 
+    /// [^num_to_read]: this is <b>not</b> the number of non-zero coords but very specifically the number to be read. 
+    /// That is an important distinction for symmetries where one coord read can turn into two non-zero coords
     pub fn write_coordinate<'a, T: Field + 'a, I: Iterator<Item = &'a (Position, T)>>(mut self, mut coord_iter: I, num_to_read: usize) -> Result<(), Error> {
         if (T::kind() == FieldKind::Pattern) & (self.symmetry != Symmetry::General) & (self.symmetry != Symmetry::Symmetric) {
             return Err(Error::UnsupportedHeaderOptions);
